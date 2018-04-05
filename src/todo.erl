b2719c765e6eda35d9597ac1c2b1d4c0df41a88d
Venkatakumar Srinivasan <venkat@xaptum.com>
2018-04-04 21:00:41 -0500

    .

--- src/todo.erl
+++ src/todo.erl
@@ -4,6 +4,5 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    rebar_api:info("IN ~p:init()", [?MODULE]),
     {ok, State1} = 'todo_prv':init(State),
     {ok, State1}.
--- src/todo_prv.erl
+++ src/todo_prv.erl
@@ -20,24 +20,20 @@ init(State) ->
             {short_desc, "example rebar3 plugin"},
             {desc, "example rebar3 plugin"}
     ]),
-    rebar_api:info("IN ~p:init()", [?MODULE]),
     {ok, rebar_state:add_provider(State, Provider)}.
 
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    rebar_api:info("IN ~p:do()", [?MODULE]),
     lists:foreach(fun check_todo_app/1, rebar_state:project_apps(State)),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
-    rebar_api:info("IN ~p:format_error()", [?MODULE]),
     io_lib:format("~p", [Reason]).
 
 
 check_todo_app(App) ->
-    rebar_api:info("IN ~p:check_todo_app()", [?MODULE]),
     Path = filename:join(rebar_app_info:dir(App),"src"),
     Mods = find_source_files(Path),
     case lists:foldl(fun check_todo_mod/2, [], Mods) of
@@ -46,11 +42,10 @@ check_todo_app(App) ->
     end.
 
 find_source_files(Path) ->
-    rebar_api:info("IN ~p:find_source_files()", [?MODULE]),
-    [filename:join(Path, Mod) || Mod <- filelib:wildcard("*.erl", Path)].
+%%    [filename:join(Path, Mod) || Mod <- filelib:wildcard("*.erl", Path)].
+    filelib:fold_files(Path, ".*erl", true, fun(F,A) -> [F|A] end, []).
 
 check_todo_mod(ModPath, Matches) ->
-    rebar_api:info("IN ~p:check_todo_mod()", [?MODULE]),
     {ok, Bin} = file:read_file(ModPath),
     case find_todo_lines(Bin) of
         [] -> Matches;
@@ -58,21 +53,17 @@ check_todo_mod(ModPath, Matches) ->
     end.
 
 find_todo_lines(File) ->
-    rebar_api:info("IN ~p:todo_lines()", [?MODULE]),
     case re:run(File, "%+.*(TODO:.*)", [{capture, all_but_first, binary}, global, caseless]) of
         {match, DeepBins} -> lists:flatten(DeepBins);
         nomatch ->  []
     end.
 
-display_todos(App, []) -> 
-    rebar_api:info("IN ~p:display_todos()", [?MODULE]),
-    io:format("Application ~s~nHas no TODOs~n",[App]),
+display_todos(_, []) -> 
     ok;
 display_todos(App, FileMatches) ->
-    rebar_api:info("IN ~p:display()", [?MODULE]),
-    io:format("Application ~s~n",[App]),
+    rebar_api:info("Application ~s",[App]),
     [begin
-      io:format("\t~s~n",[Mod]),
-      [io:format("\t  ~s~n",[TODO]) || TODO <- TODOs]
+      rebar_api:info("\t~s",[Mod]),
+      [rebar_api:warning("\t  ~s",[TODO]) || TODO <- TODOs]
      end || {Mod, TODOs} <- FileMatches],
     ok.
